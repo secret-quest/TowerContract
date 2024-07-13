98ebff511843001a9d8dadd05cc9724c00514238
Patrick C <71180317+tricodex@users.noreply.github.com>
2024-07-13 19:07:38 +0200

    Create MockERC20.sol

---
+++ src/MockERC20.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockERC20 is ERC20, Ownable {
+    constructor() ERC20("MockToken", "MKT") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    // Override to make it more gas efficient
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    // Override to make it more gas efficient
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+}
